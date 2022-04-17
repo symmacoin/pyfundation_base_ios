34f52900086f702824a122393d6a25fabdc92675
pyfundation <pyfundation.com@gmail.com>
2022-04-16 20:34:27 -0400

    proposal

--- babel.config.js
+++ babel.config.js
@@ -2,19 +2,10 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
       'module-resolver',
       {
-        extensions: [
-          '.ios.js',
-          '.android.js',
-          '.ios.jsx',
-          '.android.jsx',
-          '.js',
-          '.jsx',
-          '.json',
-          '.ts',
-          '.tsx',
-        ],
+        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
         root: ['.'],
         alias: {
           '@api': './src/api',
